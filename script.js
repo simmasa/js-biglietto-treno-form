e3b1fd07e986b466a4b75eb035f5f198651562ca
simmasa <s.masa@hotmail.it>
2022-08-02 17:32:38 +0200

    milestone 2 completa

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 const form = document.getElementById('form');
 
-form.addEventListener('submit', Ticket)
+form.addEventListener('submit', ticketMain)
 
-function Ticket (form) {
+const biglietti=[];
+
+function ticketMain (form) {
 
     form.preventDefault();
 
@@ -13,11 +15,76 @@ function Ticket (form) {
     let km = parseFloat(kmIn);
     let age = parseInt(ageIn);
 
+    /* let userPrice;
+
     const price = 0.21
     const priceUnder = price - ((price/100)*20)
     const priceOver = price - ((price/100)*40)
 
+    if (age<18) {
+        userPrice = km*priceUnder;
+    }else if (age>60) {
+        userPrice = km*priceOver;
+    } else {
+        userPrice = km*price;
+    } */
+
+    let ticketObj ={km:km,
+                    age:age,
+                    finalPrice:priceCalc(km,age)};
+
+    biglietti.push(ticketObj);
+    console.log(biglietti);
+
+    addTableRow(biglietti.length , ticketObj.km, ticketObj.age, ticketObj.finalPrice);
+
+    /* const tr = document.createElement('tr');
+    tbo.appendChild(tr);
+
+    const th = document.createElement('th');
+    tr.appendChild(th);
+    th.innerHTML = biglietti.length;
+
+    const kmtd = document.createElement('td');
+    tr.appendChild(kmtd);
+    kmtd.innerHTML= ticketObj.km;
+    const agetd = document.createElement('td');
+    tr.appendChild(agetd);
+    agetd.innerHTML = ticketObj.age;
+    const pricetd = document.createElement('td');
+    tr.appendChild(pricetd);
+    pricetd.innerHTML = ticketObj.finalPrice.toFixed(2)+'€'; */
+}
+
+function addTableRow (num,km,age,price) {
+
+    const tr = document.createElement('tr');
+    tbo.appendChild(tr);
+
+    const th = document.createElement('th');
+    tr.appendChild(th);
+    th.innerHTML = num;
+
+    const kmtd = document.createElement('td');
+    tr.appendChild(kmtd);
+    kmtd.innerHTML= km;
+    const agetd = document.createElement('td');
+    tr.appendChild(agetd);
+    agetd.innerHTML = age;
+    const pricetd = document.createElement('td');
+    tr.appendChild(pricetd);
+    pricetd.innerHTML = price.toFixed(2)+'€';
+
+}
+
+function priceCalc (km,age) {
+    
     let userPrice;
+
+    const price = 0.21
+    const priceUnder = price - ((price/100)*20)
+    const priceOver = price - ((price/100)*40)
+
     if (age<18) {
         userPrice = km*priceUnder;
     }else if (age>60) {
@@ -25,6 +92,5 @@ function Ticket (form) {
     } else {
         userPrice = km*price;
     }
-
-    console.log(userPrice);
+    return userPrice;
 }
